378e62cc1ab06a7d90b033ee101007958d263920
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-02 14:24:16 -0300

    exercicio30
    criação do exercicio 30

---
+++ exercicio30.py
@@ -0,0 +1,6 @@
+#Nicolas Andrade de Freitas - Turma 2190
+#Exercicio 20
+
+numero = float(input("Digite um valor em real:"))
+dolar= numero*5.26
+print("O valor R${} convertido em dolares é de: {:.2f}".format(numero,dolar))
