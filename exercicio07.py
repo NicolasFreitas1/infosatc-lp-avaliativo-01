d8bcb5731c66e600a8d7f8c0df7c4330d8f7e1f6
Nicolas <nicolasandrade.de.freitas@gmail.com>
2021-09-02 13:47:37 -0300

    exercicio07
    criação do exercicio 07

---
+++ exercicio07.py
@@ -0,0 +1,8 @@
+#Nicolas Andrade de Freitas - Turma 2190
+#Exercicio 07
+
+f = float(input("Digite a temperatura em fahrenheit: "))
+
+c = 5 * (f - 32 )/9
+
+print("{} Graus fahrenheit, são {} Graus Celsius".format(f, c))
